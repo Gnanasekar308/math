255b9adb58c9c87e91ec52fb99b9b26cff0d2fec
gnanasekar308 <104546066+Gnanasekar308@users.noreply.github.com>
2022-07-26 12:10:45 +0530

    Add files via upload

---
+++ math/math.js
@@ -0,0 +1,53 @@
+//math
+
+//abs()
+
+function diff(x,y){
+    return Math.abs(6+8);
+}
+console.log(diff('x,y'));
+
+//ceil()
+
+function add(a){
+    return Math.ceil(1.33);
+}
+console.log(add('a'));
+
+//floor
+
+function fl(i){
+    return Math.floor(7.63);
+}
+console.log(fl('i'));
+
+//max
+
+function ma(m,n,v){
+    return Math.max(-4,-5,-6);
+}
+console.log(ma('m,n,v'));
+
+//random()
+
+function getrandom(kumar){
+    return Math.random();
+}
+console.log(getrandom());
+
+//round
+
+function getround(c){
+    return Math.round(4.36);
+}
+console.log(getround('c'));
+
+//sqrt()
+
+function getsqrt(d){
+    return Math.sqrt(36);
+}
+console.log(getsqrt('d'));
+let min=6
+let max=9
+console.log(Math.random(5)*min*max)
